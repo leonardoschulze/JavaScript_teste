8f483ede5830ae5bd0c7adbc12de5426b96b1076
leonardoschulze <leonardo_schulze@estudante.sesisenai.org.br>
2025-03-19 09:48:33 -0300

    UPDATE - feito a calculadora

--- atividade.js
+++ atividade.js
@@ -6,4 +6,15 @@ function verficarPar(numero){
     }
 }
 
-verficarPar(4);
+verficarPar(1);
+
+
+function calcular(numero1, numero2){
+    console.log("divisão , " + (numero1 / numero2) );
+    console.log("soma, " + (numero1 + numero2) );
+    console.log("multiplicação, " + (numero1 * numero2) );
+    console.log("subtração, " + (numero1 - numero2) );
+
+}
+
+calcular(1,2);
