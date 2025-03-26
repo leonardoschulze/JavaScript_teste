780733e9e4da4e15a27775206607f38815f70cf3
leonardoschulze <leonardo_schulze@estudante.sesisenai.org.br>
2025-03-26 08:21:23 -0300

    ADD - laco while para somatório

--- atividade.js
+++ atividade.js
@@ -98,3 +98,13 @@ function converterTemperatura(temperatura){
 }
 
 converterTemperatura(10)
+
+let soma = 0;   
+let i = 1;   
+
+while (i <= 100) {   
+    soma += i;  
+    i++;   
+}  
+
+console.log("A soma dos números de 1 a 100 é: " + soma);
