63b508d89e2e4042690a758526ab825f72a65080
leonardoschulze <leonardo_schulze@estudante.sesisenai.org.br>
2025-03-26 08:07:02 -0300

    ADD - verificar negativo e positivo

--- atividade.js
+++ atividade.js
@@ -79,3 +79,16 @@ function verificarPalindromo(palavra) {
 }
 
 verificarPalindromo('arara');
+
+function verificarSinal(numero) {  
+    if (numero > 0) {  
+        console.log("Positivo");  
+    } else if (numero < 0) {  
+        console.log("Negativo");  
+    } else {  
+        console.log("Zero");  
+    }  
+}  
+
+verificarSinal(10)
+verificarSinal(-5)
